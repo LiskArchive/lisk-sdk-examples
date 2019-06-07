33f35b757320fbee66c96fc0c97d3a5409aa79eb
Vit Stanislav <vit@lightcurve.io>
2019-06-07 16:40:29 +0200

    :seedling: Create grid of the page

--- invoice/client/src/App.js
+++ invoice/client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SignInPage from './SignInPage';
 import Header from './Header';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import './App.css';
 
 function App() {
@@ -8,7 +9,13 @@ function App() {
     <React.Fragment>
       <Header />
       <div className="App">
-        <SignInPage />
+        <Grid fluid>
+          <Row  center="xs">
+            <Col xs={12} md={8} lg={6}>
+              <SignInPage />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     </React.Fragment>
   );
--- invoice/client/src/SignInPage.js
+++ invoice/client/src/SignInPage.js
@@ -4,21 +4,26 @@ import {
   Form, FormGroup, Label, Input,
   Card, CardHeader, CardBody,
 } from 'reactstrap';
+import { Row, Col } from 'react-flexbox-grid';
 
 function SignInPage() {
   return (
-   <Card>
-    <CardHeader>Sign In</CardHeader>
-    <CardBody>
-      <Form>
-        <FormGroup>
-          <Label for="passphrase">passphrase</Label>
-          <Input type="password" name="passphrase" id="passphrase" placeholder="Enter your 12 word Lisk passphrase" />
-        </FormGroup>
-        <Button color="primary">Sign In</Button>
-      </Form>
-    </CardBody>
-   </Card>
+    <Row start="xs">
+      <Col xs={12}>
+        <Card>
+          <CardHeader><h3>Sign In</h3></CardHeader>
+          <CardBody>
+            <Form>
+              <FormGroup>
+                <Label for="passphrase">Passphrase</Label>
+                <Input type="password" name="passphrase" id="passphrase" placeholder="Enter your 12 word Lisk passphrase" />
+              </FormGroup>
+              <Button color="primary" size="lg" block>Sign In</Button>
+            </Form>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
   );
 }
 
