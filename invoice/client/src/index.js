32b08e0c0df3b7e5d1bbc6c6b36fda5f2e07c834
Vit Stanislav <vit@lightcurve.io>
2019-06-11 07:39:14 +0200

    :recycle: Create components folder for components

--- invoice/client/src/index.js
+++ invoice/client/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
