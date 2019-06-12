2a6685a2288bda939d3b2f768dbb9a7b04aa3e97
Vit Stanislav <vit@lightcurve.io>
2019-06-12 18:24:46 +0200

    :seedling: Implement global state management

--- invoice/client/src/components/App.js
+++ invoice/client/src/components/App.js
@@ -1,30 +1,34 @@
-import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from 'react';
 
-import SignInPage from './SignInPage';
-import InvoicesPage from './InvoicesPage';
-import SendInvoicePage from './SendInvoicePage';
-import PayInvoicePage from './PayInvoicePage';
+import { StateProvider } from '../state';
 import Header from './Header';
+import InvoicesPage from './InvoicesPage';
+import PayInvoicePage from './PayInvoicePage';
+import SendInvoicePage from './SendInvoicePage';
+import SignInPage from './SignInPage';
+
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="App">
-        <Grid fluid>
-          <Row center="xs">
-            <Col xs={12} md={10} lg={8}>
-              <Route path="/" exact component={SignInPage} />
-              <Route path="/invoices" component={InvoicesPage} />
-              <Route path="/send-invoice" component={SendInvoicePage} />
-              <Route path="/pay-invoice" component={PayInvoicePage} />
-            </Col>
-          </Row>
-        </Grid>
-      </div>
+      <StateProvider>
+        <Header />
+        <div className="App">
+          <Grid fluid>
+            <Row center="xs">
+              <Col xs={12} md={10} lg={8}>
+                <Route path="/" exact component={SignInPage} />
+                <Route path="/invoices" component={InvoicesPage} />
+                <Route path="/send-invoice" component={SendInvoicePage} />
+                <Route path="/pay-invoice" component={PayInvoicePage} />
+              </Col>
+            </Row>
+          </Grid>
+        </div>
+      </StateProvider>
     </Router>
   );
 }
