d4342b1c2d5b0541d52a1ab9fb49e9fdcf1fb6d6
Vit Stanislav <vit@lightcurve.io>
2019-06-12 12:30:10 +0200

    :seedling: Implement form validation of SendInvoicePage

--- invoice/client/src/components/SendInvoicePage.js
+++ invoice/client/src/components/SendInvoicePage.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import {
   Button,
-  Form, FormGroup, Label, Input,
+  Form, FormGroup, Label, Input, FormFeedback,
   Card, CardHeader, CardBody,
 } from 'reactstrap';
 import { Row, Col } from 'react-flexbox-grid';
 import { Link } from 'react-router-dom';
 
+
 function SendInvoicePage() {
+  const inputs = {
+    address: {
+      label: 'Client address',
+      isValid: value => value.match(/^[1-9]\d{0,19}L$/),
+    },
+    amount: {
+      label: 'Amount',
+      isValid: value => value.match(/^\d+(\.\d{1,8})?$/),
+    },
+    description: {
+      label: 'Description',
+      isValid: () => true,
+    },
+  };
+
+  const [state, setState] = React.useState({
+    inputs: Object.keys(inputs).reduce((accumulator, key) => {
+      accumulator[key] = { // eslint-disable-line no-param-reassign
+        value: '',
+        error: '',
+      };
+      return accumulator;
+    }, {}),
+  });
+
+  const onInputChange = (inputName, event) => {
+    const { value } = event.target;
+    setState({
+      inputs: {
+        ...state.inputs,
+        [inputName]: {
+          value,
+          error: inputs[inputName].isValid(value) ? '' : 'Invalid value',
+        },
+      },
+    });
+  };
+
   return (
     <Row start="xs">
       <Col xs={12}>
@@ -17,18 +56,19 @@ function SendInvoicePage() {
           </CardHeader>
           <CardBody>
             <Form>
-              <FormGroup>
-                <Label for="address">Client address</Label>
-                <Input type="text" name="address" id="address" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="amount">Amount (LSK)</Label>
-                <Input type="text" name="amount" id="amount" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="description">Description</Label>
-                <Input type="text" name="description" id="description" />
-              </FormGroup>
+              {Object.entries(inputs).map(([key, { label }]) => (
+                <FormGroup key={key}>
+                  <Label for={key}>{label}</Label>
+                  <Input
+                    type="text"
+                    id={key}
+                    value={state.inputs[key].value}
+                    invalid={state.inputs[key].error !== ''}
+                    onChange={event => onInputChange(key, event)}
+                  />
+                  <FormFeedback>{state.inputs[key].error}</FormFeedback>
+                </FormGroup>
+              ))}
               <Row between="xs">
                 <Col xs={5}>
                   <Link to="/invoices">
