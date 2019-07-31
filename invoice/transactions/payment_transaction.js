deebda5a3e2fead9f245d7cce83d0c4c36f94aad
Mona Baerenfaenger <mona@lightcurve.io>
2019-07-31 15:06:05 +0200

    Update sdk npm package in manifests

--- invoice/transactions/payment_transaction.js
+++ invoice/transactions/payment_transaction.js
@@ -15,44 +15,38 @@ class PaymentTransaction extends TransferTransaction {
 	 }
 
 	applyAsset(store) {
-		super.applyAsset(store);
-		const errors = [];
 		const transaction = store.transaction.find(
 			transaction => transaction.id === this.asset.data
 		); // Find related invoice in transactions for invoiceID
 
-		if (transaction) {
-			if (this.amount.lt(transaction.asset.requestedAmount)) {
-				errors.push(
-					new TransactionError(
-						'Paid amount is lower than amount stated on invoice',
-						this.id,
-						'.amount',
-						transaction.requestedAmount,
-						'Expected amount to be equal or greated than `requestedAmount`',
-					)
-				);
-			}
-		} else {
-			errors.push(
-				new TransactionError(
+		if (!transaction) {
+            return [ new TransactionError(
 					'Invoice does not exist for ID',
 					this.id,
 					'.asset.invoiceID',
 					this.asset.data,
 					'Existing invoiceID registered as invoice transaction',
-				)
-			);
+				) ];
 		}
-
-		return errors;
+        if (this.amount.lt(transaction.asset.requestedAmount)) {
+            return [ new TransactionError(
+                'Paid amount is lower than amount stated on invoice',
+                this.id,
+                '.amount',
+                transaction.requestedAmount,
+                'Expected amount to be equal or greated than `requestedAmount`',
+            )];
+        }
+        this.recipientId = transaction.senderId;
+        super.applyAsset(store);
+        return [];
 	}
 
 	undoAsset(store) {
 		// No rollback needed as there is only validation happening in applyAsset
 		// Higher level function will rollback the attempted payment (send back tokens)
-		super.undoAsset(store); 
-	
+		super.undoAsset(store);
+
 		return [];
 	}
 
