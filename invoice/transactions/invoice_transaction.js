dbbf07d64bc3c191789040080aa70625753d9821
Mona Baerenfaenger <mona@lightcurve.io>
2019-07-31 11:23:41 +0200

    Set invoicesSent to undefined in undoAsset, if the list is empty

--- invoice/transactions/invoice_transaction.js
+++ invoice/transactions/invoice_transaction.js
@@ -70,10 +70,10 @@ class InvoiceTransaction extends BaseTransaction {
 
 		// Rollback invoice count and IDs
 		sender.asset.invoiceCount = sender.asset.invoiceCount === 1 ? undefined : sender.asset.invoiceCount--;
-		sender.asset.invoicesSent = sender.asset.invoicesSent.splice(
-			sender.asset.invoicesSent.indexOf(this.id),
-			1,
-		);
+		sender.asset.invoicesSent = sender.asset.invoicesSent.length === 1 ? undefined : sender.asset.invoicesSent.splice(
+            sender.asset.invoicesSent.indexOf(this.id),
+            1,
+        );
 		store.account.set(sender.address, sender);
 		return [];
 	}
