25ddecd781450df1954d9908fea2a39b38340094
Mona Baerenfaenger <mona@lightcurve.io>
2019-10-02 16:59:08 +0200

    Create client scripts for (un)registerCarrier transactions

--- delivery/client/print_sendable_register_carrier.js
+++ delivery/client/print_sendable_register_carrier.js
@@ -1,5 +1,4 @@
-const RegisterPacketTransaction = require('../transactions/register-packet');
-const transactions = require('@liskhq/lisk-transactions');
+const RegisterCarrierTransaction = require('../transactions/register-carrier');
 const { EPOCH_TIME } = require('@liskhq/lisk-constants');
 
 const getTimestamp = () => {
@@ -16,17 +15,11 @@ const packetCredentials = { address: '5420762878027534930L',
         '1529b602ea69497ff5e38c3d1db5e90f9dfb6ee2f0e83534c89f18579a24653b',
     privateKey:
         'f3058da026f8b1a93643dc6864fc5ef7830b7614dd09292bc5f2ca3d4b115f9e1529b602ea69497ff5e38c3d1db5e90f9dfb6ee2f0e83534c89f18579a24653b'
-}; // insert here the packetCredentials-object created in create_and_initialize_packet_account.js
+ }; // insert here the packetCredentials-object created in create_and_initialize_packet_account.js
 
-let tx =  new RegisterPacketTransaction({
+let tx =  new RegisterCarrierTransaction({
     asset: {
-        packetId: packetCredentials.address,
-        senderLocation: "def alley, 456 someCity",
-        receipientLocation: "abc street, 123 someCity",
-        porto: `${transactions.utils.convertLSKToBeddows('5')}`,
-        security: `${transactions.utils.convertLSKToBeddows('100')}`,
-        minTrust: 25,
-        estTravelTime: "18000", // 18,000 seconds = 5 hrs
+        packetId: packetCredentials.address
     },
     fee: "0",
     recipientId: "10881167371402274308L",
