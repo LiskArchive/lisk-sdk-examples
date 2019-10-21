a92414a61c77247cf957daccf9fdbfbd639aaf09
Mona Baerenfaenger <mona@lightcurve.io>
2019-10-21 17:41:04 +0200

    Add initialize account page to explorer

--- transport/explorer/app.js
+++ transport/explorer/app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const { APIClient } = require('@liskhq/lisk-api-client');
 const RegisterPacketTransaction = require('../transactions/register-packet');
 const LightAlarmTransaction = require('../transactions/light-alarm');
+const transactions = require('@liskhq/lisk-transactions');
+const cryptography = require('@liskhq/lisk-cryptography');
+const { Mnemonic } = require('@liskhq/lisk-passphrase');
 
 // Constants
 const API_BASEURL = 'http://localhost:4000';
@@ -75,13 +78,13 @@ app.get('/register-packet/:senderId', async(req, res) => {
  */
 app.get('/light-alarm', async(req, res) => {
     const { data: transactions } = await api.transactions.get({ type: LightAlarmTransaction.TYPE });
-    
+
     // Sort desc
     transactions.sort((a, b) => {
         if (a.timestamp > b.timestamp) return -1;
-    
+
         if (a.timestamp < b.timestamp) return 1;
-    
+
         if (a.timestamp === b.timestamp) return 0;
     });
 
@@ -93,17 +96,70 @@ app.get('/light-alarm', async(req, res) => {
  */
 app.get('/light-alarm/:senderId', async(req, res) => {
     const { data: transactions } = await api.transactions.get({ type: LightAlarmTransaction.TYPE, senderId: req.params.senderId });
-    
+
     // Sort desc
     transactions.sort((a, b) => {
         if (a.timestamp > b.timestamp) return -1;
-    
+
         if (a.timestamp < b.timestamp) return 1;
-    
+
         if (a.timestamp === b.timestamp) return 0;
     });
 
     res.render('light-alarm', { transactions });
 });
+
+app.get('/initialize', async(req, res) => {
+    //console.log(req.body);
+   // res.send(req.body);
+
+    const getPacketCredentials = () => {
+        const passphrase = Mnemonic.generateMnemonic();
+        const keys = cryptography.getPrivateAndPublicKeyFromPassphrase(
+            passphrase
+        );
+        const credentials = {
+            address: cryptography.getAddressFromPublicKey(keys.publicKey),
+            passphrase: passphrase,
+            publicKey: keys.publicKey,
+            privateKey: keys.privateKey
+        };
+        return credentials;
+    };
+
+    const packetCredentials = getPacketCredentials();
+
+    let tx = new transactions.TransferTransaction({
+        amount: '1',
+        recipientId: packetCredentials.address,
+    });
+
+    tx.sign('wagon stock borrow episode laundry kitten salute link globe zero feed marble'); // Genesis account with address: 16313739661670634666L
+    res.render('initialize', { packetCredentials });
+
+    api.transactions.broadcast(tx.toJSON()).then(res => {
+        console.log("++++++++++++++++ API Response +++++++++++++++++");
+        console.log(res.data);
+        console.log("++++++++++++++++ Credentials +++++++++++++++++");
+        console.dir(packetCredentials);
+        console.log("++++++++++++++++ Transaction Payload +++++++++++++++++");
+        console.log(tx.stringify());
+        console.log("++++++++++++++++ End Script +++++++++++++++++");
+    }).catch(err => {
+        console.log(JSON.stringify(err.errors, null, 2));
+    });
+
+
+
+    res.end()
+});
+
+app.post('/post-register-packet', function (req, res) {
+    console.log(req.body);
+    res.send(req.body);
+
+    res.end()
+});
+
 
 app.listen(PORT, () => console.info(`Explorer app listening on port ${PORT}!`));
