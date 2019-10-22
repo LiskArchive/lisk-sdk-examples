707c40b7e3f4254f9a52ac41dd2f9d314c2e51a4
yatki <mehmet@yatki.com>
2019-10-22 15:40:53 +0200

    some code refactorings

--- workshop/index.js
+++ workshop/index.js
@@ -1,16 +1,18 @@
-const { Application, genesisBlockDevnet, configDevnet } = require('lisk-sdk');
+const { Application, genesisBlockDevnet, configDevnet } = require("lisk-sdk");
+// @todo uncomment these accordingly, during the workshop
 // const { InvoiceTransaction } = require('./transactions/index');
 // const { PaymentTransaction } = require('./transactions/index');
 
 const app = new Application(genesisBlockDevnet, configDevnet);
 
+// @todo uncomment these accordingly, during the workshop
 // app.registerTransaction(InvoiceTransaction);
-// app.registerTransaction(PaymentTransaction); 
+// app.registerTransaction(PaymentTransaction);
 
 app
-	.run()
-	.then(() => app.logger.info('App started...'))
-	.catch(error => {
-		console.error('Faced error in application', error);
-		process.exit(1);
-	});
+  .run()
+  .then(() => app.logger.info("App started..."))
+  .catch(error => {
+    console.error("Faced error in application", error);
+    process.exit(1);
+  });
