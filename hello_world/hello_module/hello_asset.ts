d3b2bb0f851ec928b0be56cf8085cf8f7345c657
Mona Baerenfaenger <mona@lightcurve.io>
2020-10-30 18:23:01 +0100

    Add hello module

---
+++ hello_world/hello_module/hello_asset.ts
@@ -0,0 +1,80 @@
+const {
+    BaseAsset,
+    ValidationError,
+} = require('lisk-sdk');
+
+interface Asset {
+    Data: string;
+}
+export class HelloAsset extends BaseAsset {
+    name = 'helloAsset';
+    id = 0;
+    schema = {
+        $id: '/helloAsset',
+        type: 'object',
+        properties: {
+            hello: {
+                dataType: 'string',
+                fieldNumber: 1,
+            },
+        },
+    };
+
+    validate({asset}) {
+        if (!asset.hello || typeof asset.hello !== 'string' || asset.hello.length > 64) {
+            throw new ValidationError(
+                'Invalid "asset.hello" defined on transaction: A string value no longer than 64 characters',
+                asset.hello,
+            );
+        }
+    };
+
+    async apply({ asset, stateStore, reducerHandler, transaction }) {
+        const senderAddress = transaction.senderAddress;
+        const senderAccount = await stateStore.account.get(senderAddress);
+
+        if (senderAccount.asset && senderAccount.asset.hello) {
+            throw new InvalidTransactionError(
+                'You cannot send a hello transaction multiple times',
+                transaction.id
+            );
+        } else {
+            sender.asset = { hello: this.asset.hello };
+            store.account.set(sender.address, sender);
+        }
+
+        const senderBalance = await reducerHandler.invoke("token:getBalance", {
+            address: senderAddress,
+        });
+        const minRemainingBalance = await reducerHandler.invoke(
+            "token:getMinRemainingBalance"
+        );
+
+        if (asset.initValue < minRemainingBalance) {
+            throw new Error("NFT init value is too low.");
+        }
+
+        if (senderBalance < asset.initValue + minRemainingBalance) {
+            throw new Error("Sender balance is not enough to create an NFT");
+        }
+
+        const nftToken = createNFTToken({
+            ownerAddress: senderAddress,
+            nonce: transaction.nonce,
+            value: asset.initValue,
+            minPurchaseMargin: asset.minPurchaseMargin,
+        });
+
+        senderAccount.nft.ownNFTs.push(nftToken.id);
+        await stateStore.account.set(senderAddress, senderAccount);
+
+        await reducerHandler.invoke("token:debit", {
+            address: senderAddress,
+            amount: asset.initValue,
+        });
+
+        const allTokens = await getAllNFTTokens(stateStore);
+        allTokens.push(nftToken);
+        await setAllNFTTokens(stateStore, allTokens);
+    }
+}
---
+++ hello_world/hello_module/hello_module.js
@@ -0,0 +1,87 @@
+const {
+    BaseModule,
+    TransactionApplyContext,
+    AfterBlockApplyContext,
+    BeforeBlockApplyContext,
+    AfterGenesisBlockApplyContext,
+} = require('lisk-sdk');
+const { CustomAsset } = require('./custom_asset');
+
+export class HelloModule extends BaseModule {
+    name = 'hello';
+    id = 1000;
+    accountSchema = {
+        type: 'object',
+        properties: {
+            hello: {
+                fieldNumber: 1,
+                dataType: 'string',
+            },
+        },
+        default: {
+            hello: '',
+        },
+    };
+    transactionAssets: [new CustomAsset()];
+    actions = {
+        someAction: async () => {
+            return this.id
+        },
+    };
+    events = ['someEvent','anotherEvent'];
+    reducers = {};
+    beforeTransactionApply(context: TransactionApplyContext): Promise<void> {
+        // Code in here is applied before a transaction is applied.
+        this._channel.publish('hello:someEvent', { info: 'sample' });
+    };
+    afterTransactionApply(context: TransactionApplyContext): Promise<void> {
+        // Code in here is applied after a transaction is applied.
+    };
+    afterGenesisBlockApply(context: AfterGenesisBlockApplyContext): Promise<void> {
+        // Code in here is applied after a genesis block is applied.
+    };
+    beforeBlockApply(context: BeforeBlockApplyContext): Promise<void> {
+        // Code in here is applied before a block is applied.
+    }
+    afterBlockApply(context: AfterBlockApplyContext): Promise<void> {
+        // Code in here is applied after a block is applied.
+        this._channel.subscribe('app:chain:fork ', ({ data }) => {
+            console.log(data);
+        });
+    }
+}
+
+class NFTModule extends BaseModule {
+    name = "nft";
+    id = 1000;
+    transactionAssets = [new CreateNFT(), new TransferNFT(), new PurchaseNFT()];
+
+    accountSchema = {
+        type: "object",
+        required: ["hello"],
+        properties: {
+            hello: {
+                type: "string",
+                fieldNumber: 1,
+            },
+        },
+        default: {
+            hello: "",
+        },
+    };
+
+    actions = {
+        helloAction: async () => {
+            return this._dataAccess
+        },
+    };
+
+    events = {
+        helloAction: async () => {
+            return this._dataAccess
+        },
+    };
+}
+
+
+module.exports = HelloModule;
