7579a64e7c3df8c6481b380db7e4498350d0ac2c
Mona Baerenfaenger <mona@lightcurve.io>
2021-03-01 18:05:38 +0100

    Merge srs_module.js into index.js

--- tutorials/social-recovery/blockchain_app/srs_module/index.js
+++ tutorials/social-recovery/blockchain_app/srs_module/index.js
@@ -1,3 +1,27 @@
-const SRSModule = require('./srs_module');
+const { BaseModule } = require('lisk-sdk');
+const CreateRecovery = require('./assets/create_recovery');
+const InitiateRecovery = require('./assets/initiate_recovery');
+const VouchRecovery = require('./assets/vouch_recovery');
+const ClaimRecovery = require('./assets/claim_recovery');
+const CloseRecovery = require('./assets/close_recovery');
+const RemoveRecovery = require('./assets/remove_recovery');
+const SRSAccountSchema = require('./schema');
+
+// Extend from the base module to implement a custom module
+class SRSModule extends BaseModule {
+  name = 'srs';
+  id = 1000;
+  accountSchema = SRSAccountSchema;
+
+  transactionAssets = [
+    new CreateRecovery(),
+    new InitiateRecovery(),
+    new VouchRecovery(),
+    new ClaimRecovery(),
+    new CloseRecovery(),
+    new RemoveRecovery(),
+  ];
+
+}
 
 module.exports = { SRSModule };
