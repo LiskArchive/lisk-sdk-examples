4309e9101492f07ff3b5fa8d3d272f6c9dc2e08a
Mona Baerenfaenger <mona@lightcurve.io>
2021-07-06 13:29:36 +0200

    Update module guide

--- guides/02-module/hello_app/src/app/modules/hello/hello_module.ts
+++ guides/02-module/hello_app/src/app/modules/hello/hello_module.ts
@@ -18,20 +18,45 @@
 
 import {
     BaseModule,
+    codec,
     AfterBlockApplyContext,
     TransactionApplyContext,
     BeforeBlockApplyContext,
     AfterGenesisBlockApplyContext,
     // GenesisConfig
 } from 'lisk-sdk';
+const {
+    helloCounterSchema,
+    helloAssetSchema,
+    CHAIN_STATE_HELLO_COUNTER
+} = require('./schemas');
 // Uncomment once the asset is implemented
 // const { HelloAsset } = require('./assets/hello_asset');
 
 export class HelloModule extends BaseModule {
+    public accountSchema = {
+        type: 'object',
+        properties: {
+            helloMessage: {
+                fieldNumber: 1,
+                dataType: 'string',
+                minLength: 3,
+                maxLength: 64,
+            },
+        },
+        default: {
+            helloMessage: '',
+        },
+    };
     public actions = {
-        // Example below
-        // getBalance: async (params) => this._dataAccess.account.get(params.address).token.balance,
-        // getBlockByID: async (params) => this._dataAccess.blocks.get(params.id),
+        amountOfHellos: async () => {
+            const res = await this._dataAccess.getChainState(CHAIN_STATE_HELLO_COUNTER);
+            const count = codec.decode(
+                helloCounterSchema,
+                res
+            );
+            return count;
+        },
     };
     public reducers = {
         // Example below
@@ -52,10 +77,7 @@ export class HelloModule extends BaseModule {
         // Uncomment once the asset is implemented
         // new HelloAsset()
     ];
-    public events = [
-        // Example below
-        // 'hello:newBlock',
-    ];
+    public events = ['newHello'];
     public id = 1000;
 
     // public constructor(genesisConfig: GenesisConfig) {
@@ -63,30 +85,28 @@ export class HelloModule extends BaseModule {
     // }
 
     // Lifecycle hooks
-    public async beforeBlockApply(_input: BeforeBlockApplyContext) {
-        // Get any data from stateStore using block info, below is an example getting a generator
-        // const generatorAddress = getAddressFromPublicKey(_input.block.header.generatorPublicKey);
-		// const generator = await _input.stateStore.account.get<TokenAccount>(generatorAddress);
-    }
-
-    public async afterBlockApply(_input: AfterBlockApplyContext) {
-        // Get any data from stateStore using block info, below is an example getting a generator
-        // const generatorAddress = getAddressFromPublicKey(_input.block.header.generatorPublicKey);
-		// const generator = await _input.stateStore.account.get<TokenAccount>(generatorAddress);
-    }
-
-    public async beforeTransactionApply(_input: TransactionApplyContext) {
-        // Get any data from stateStore using transaction info, below is an example
-        // const sender = await _input.stateStore.account.getOrDefault<TokenAccount>(_input.transaction.senderAddress);
-    }
-
     public async afterTransactionApply(_input: TransactionApplyContext) {
-        // Get any data from stateStore using transaction info, below is an example
-        // const sender = await _input.stateStore.account.getOrDefault<TokenAccount>(_input.transaction.senderAddress);
+        // Publish a `newHello` event for every received hello transaction
+        // Uncomment the snippet below, after the hello asset was created (explained in the next guide)
+        /*if (transaction.moduleID === this.id && transaction.assetID === HelloAssetID) {
+
+          const helloAsset = codec.decode(
+            helloAssetSchema,
+            transaction.asset
+          );
+
+          this._channel.publish('hello:newHello', {
+            sender: transaction._senderAddress.toString('hex'),
+            hello: helloAsset.helloString
+          });
+        }*/
     }
 
     public async afterGenesisBlockApply(_input: AfterGenesisBlockApplyContext) {
-        // Get any data from genesis block, for example get all genesis accounts
-        // const genesisAccoounts = genesisBlock.header.asset.accounts;
+        // Set the hello counter to zero after the genesis block is applied
+        await stateStore.chain.set(
+            CHAIN_STATE_HELLO_COUNTER,
+            codec.encode(helloCounterSchema, { helloCounter: 0 })
+        );
     }
 }
---
+++ guides/02-module/hello_app/src/app/modules/hello/schemas.js
@@ -0,0 +1,31 @@
+const CHAIN_STATE_HELLO_COUNTER = "hello:helloCounter";
+
+const helloCounterSchema = {
+  $id: "lisk/hello/counter",
+  type: "object",
+  required: ["helloCounter"],
+  properties: {
+    helloCounter: {
+      dataType: "uint32",
+      fieldNumber: 1,
+    },
+  },
+};
+
+const helloAssetSchema = {
+  $id: "lisk/hello/asset",
+  type: "object",
+  required: ["helloString"],
+  properties: {
+    helloString: {
+      dataType: "string",
+      fieldNumber: 1,
+    },
+  },
+};
+
+module.exports = {
+  CHAIN_STATE_HELLO_COUNTER,
+  helloCounterSchema,
+  helloAssetSchema
+};
