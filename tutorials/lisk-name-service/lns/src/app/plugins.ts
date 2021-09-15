06916b3f14061bf72a8218ba203ae93097b34e05
Mona Baerenfaenger <mona@lightcurve.io>
2021-09-15 11:41:53 +0200

    Updatee lns app

--- tutorials/lisk-name-service/lns/src/app/plugins.ts
+++ tutorials/lisk-name-service/lns/src/app/plugins.ts
@@ -1,9 +1,13 @@
 import { Application, ForgerPlugin, HTTPAPIPlugin } from 'lisk-sdk';
 import { LNSDashboardPlugin } from 'lns-dashboard-plugin';
+import { DashboardPlugin } from "@liskhq/lisk-framework-dashboard-plugin";
+import { FaucetPlugin } from "@liskhq/lisk-framework-faucet-plugin";
 
 export const registerPlugins = (app: Application): void => {
 	app.registerPlugin(HTTPAPIPlugin);
 	app.registerPlugin(ForgerPlugin);
+    app.registerPlugin(DashboardPlugin);
+    app.registerPlugin(FaucetPlugin);
 	app.registerPlugin(LNSDashboardPlugin);
 
 	app.overridePluginOptions(LNSDashboardPlugin.alias, {
