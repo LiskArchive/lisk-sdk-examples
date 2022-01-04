ba2354f2694705208637bfd6fbf0a14400c83433
Tschakki <tschakki@posteo.de>
2022-01-04 13:58:20 +0100

    Add encoding tx examples

--- guides/decoding-blocks-transactions/getTxById.js
+++ guides/decoding-blocks-transactions/getTxById.js
@@ -19,10 +19,18 @@ getClient().then((client) => {
 	client.invoke("app:getTransactionByID", {
 		id: txId
 	}).then(res => {
-		console.log(res);
-		const txObject = client.transaction.decode(res);
-		const txJSON = client.transaction.toJSON(txObject);
-		console.log(txJSON);
+		const decodedTx = client.transaction.decode(res);
+		const txJSON = client.transaction.toJSON(decodedTx);
+		const txBuffer = client.transaction.fromJSON(txJSON);
+		const encodedTxAsBuffer = client.transaction.encode(txBuffer)
+		const encodedTxAsHexString = encodedTxAsBuffer.toString('hex')
+		console.log("Encoded tx: ", res)
+		console.log("Decoded tx: ", decodedTx);
+		console.log("Tx as JSON: ", txJSON);
+		console.log("Tx from JSON to Buffer: ", txBuffer);
+		console.log("Re-encoded tx: ", encodedTxAsBuffer);
+		console.log("Encoded tx as hex string: ", encodedTxAsHexString)
+		console.log("res = encodedTxAsHexString? - ", (res == encodedTxAsHexString));
 		process.exit(0);
 	});
 });
