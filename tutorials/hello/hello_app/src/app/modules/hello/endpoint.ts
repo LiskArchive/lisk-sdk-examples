f94a5ba87040f5d98ffb34790d41ad930a69081f
Tschakki <tschakki@posteo.de>
2022-10-06 17:52:18 +0200

    Add getHello() endpoint

--- tutorials/hello/hello_app/src/app/modules/hello/endpoint.ts
+++ tutorials/hello/hello_app/src/app/modules/hello/endpoint.ts
@@ -1,5 +1,6 @@
-import { BaseEndpoint, ModuleEndpointContext, codec } from 'lisk-sdk';
+import { BaseEndpoint, ModuleEndpointContext, cryptography } from 'lisk-sdk';
 import { CounterStore, CounterStoreData } from './stores/counter';
+import { MessageStore, MessageStoreData } from './stores/message';
 
 export class HelloEndpoint extends BaseEndpoint {
 
@@ -11,6 +12,21 @@ export class HelloEndpoint extends BaseEndpoint {
 			Buffer.from('hello','utf8'),
 		);
 
+		return helloCounterData;
+	}
+
+	public async getHello(ctx: ModuleEndpointContext): Promise<MessageStoreData> {
+		const counterSubStore = this.stores.get(MessageStore);
+
+		const { address } = ctx.params;
+		if (typeof address !== 'string') {
+			throw new Error('Parameter address must be a string.');
+		}
+		cryptography.address.validateLisk32Address(address);
+		const helloCounterData = await counterSubStore.get(
+			ctx,
+			cryptography.address.getAddressFromLisk32Address(address),
+		);
 		return helloCounterData;
 	}
 }
