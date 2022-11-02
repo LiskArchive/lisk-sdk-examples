ca0eeb7c7b8bd1f92394a0ac7a91d62a960562b1
Tschakki <tschakki@posteo.de>
2022-11-02 10:57:03 +0100

    Remove gitbkp

--- tutorials/hello/hello_client/gitbkp/hooks/husky.local.sh
+++
@@ -1,6 +0,0 @@
-# Created by Husky v4.2.5 (https://github.com/typicode/husky#readme)
-#   At: 10/26/2022, 1:23:07 PM
-#   From: /Users/mona/git/lisk-sdk-examples/tutorials/hello/hello_client/node_modules/husky (undefined)
-
-packageManager=npm
-cd "."
