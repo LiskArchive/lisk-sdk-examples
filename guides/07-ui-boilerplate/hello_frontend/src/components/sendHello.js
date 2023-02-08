a452111ef5b49d2c6d846c06d4de80d4f64cefb7
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2023-02-08 12:27:30 +0500

    Update sendHello.js

--- guides/07-ui-boilerplate/hello_frontend/src/components/sendHello.js
+++ guides/07-ui-boilerplate/hello_frontend/src/components/sendHello.js
@@ -1,31 +1,46 @@
 import FixedMenuLayout from '../layout/header';
-import { Form, Button, Grid } from 'semantic-ui-react';
+import { Form, Button, Container, Divider } from 'semantic-ui-react';
 
 function SendHello() {
     return (
-        <div className="App">
+        <>
             <FixedMenuLayout />
-            <div>
-                <h1>Send Hello Message</h1>
-                <p>Send a Hello transaction.</p>
-                <Grid textAlign='center' style={{ height: 'max', overflow: 'hidden' }} verticalAlign='middle'>
-                    <Grid.Column style={{ maxWidth: 500 }}>
-                        <Form>
-                            <Form.Field>
-                                <input placeholder="Hello Message" />
-                            </Form.Field>
-                            <Form.Field>
-                                <input placeholder='Fee' />
-                            </Form.Field>
-                            <Form.Field>
-                                <input placeholder='Passphrase' />
-                            </Form.Field>
-                            <Button type='submit' fluid size='large' style={{ backgroundColor: '#2BD67B', color: 'white' }}>Submit</Button>
-                        </Form>
-                    </Grid.Column>
-                </Grid>
-            </div>
-        </div>
+            <Container>
+                <div>
+                    <h1>Send Hello Message</h1>
+                    <p>Please fill the following form to send a "Hello" message.</p>
+                    <Divider></Divider>
+                    <div class="ui two column doubling stackable grid container">
+                        <div class="column">
+                            <Form class="ui form">
+                                <Form.Field class="field">
+                                    <label>Your message:</label>
+                                    <input placeholder="Hello Message" id="hello" name="hello" />
+                                </Form.Field >
+                                <Form.Field class="field">
+                                    <label>Fee:</label>
+                                    <input placeholder='Fee (1 = 10^8 tokens)' type="text" id="fee" name="fee" />
+                                </Form.Field>
+                                <Form.Field class="field">
+                                    <label>Sender's private key:</label>
+                                    <input placeholder="Private key of sender's account" type="password" id="privateKey" name="privateKey" />
+                                </Form.Field>
+                                <Button type='submit' fluid size='large' style={{ backgroundColor: '#2BD67B', color: 'white' }}>Submit</Button>
+                            </Form>
+                        </div>
+
+                        <div className='column'>
+                            <h3>Your transaction's details are:</h3>
+                            <div class="ui raised segment" style={{ overflow: 'scroll' }}>
+                                <>
+
+                                </>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </>
     );
 }
 
