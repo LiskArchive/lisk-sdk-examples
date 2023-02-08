30a09dd118de846064e8a9a078b519da513835a9
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2023-02-08 12:26:23 +0500

    Update newAccount.js

--- guides/07-ui-boilerplate/hello_frontend/src/components/newAccount.js
+++ guides/07-ui-boilerplate/hello_frontend/src/components/newAccount.js
@@ -1,21 +1,26 @@
 import FixedMenuLayout from '../layout/header';
-import { Form, Button, Grid } from 'semantic-ui-react';
+import { Container, Divider } from 'semantic-ui-react';
 
 function NewAccount() {
     return (
-        <div className="App">
+        <div>
             <FixedMenuLayout />
-            <div>
-                <h2>Create new account</h2>
-                <p>To create a new account, click on the following button:</p>
-                <Grid textAlign='center' style={{ height: 'max', overflow: 'hidden' }} verticalAlign='middle'>
-                    <Grid.Column style={{ maxWidth: 500 }}>
-                        <Form>
-                            <Button type='submit' fluid size='large' style={{ backgroundColor: '#2BD67B', color: 'white' }}>Click Me</Button>
-                        </Form>
-                    </Grid.Column>
-                </Grid>
-            </div>
+            <Container>
+
+                <div>
+
+                    <div class="ui green segment">
+                        <i class="lightbulb outline icon"></i><strong>TIP:</strong> Reload the page to generate a new account.
+                    </div>
+
+                    <h2>New account created!</h2>
+                    <Divider></Divider>
+                    <div className='App'>
+
+                    </div>
+                </div>
+
+            </Container>
         </div>
     );
 }
