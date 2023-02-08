0da52fd8c9ed6388d1f439ce4b11e4f50e58b657
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2023-02-08 12:15:47 +0500

    Refactor code
    - Update package.json
    - Update faucet.js

--- guides/07-ui-boilerplate/hello_frontend/src/components/faucet.js
+++ guides/07-ui-boilerplate/hello_frontend/src/components/faucet.js
@@ -1,27 +1,45 @@
 import FixedMenuLayout from '../layout/header';
-import React from 'react';
-import { Form, Button, Grid } from 'semantic-ui-react';
+import { Form, Button, Divider, Container } from 'semantic-ui-react';
 
 function Faucet() {
     return (
-        <div className="App">
+        <>
             <FixedMenuLayout />
-            <h1>Faucet</h1>
-            <h4>The faucet transfers tokens from the genesis account to another.</h4>
-            <Grid textAlign='center' style={{ height: 'max', overflow: 'hidden' }} verticalAlign='middle'>
-                <Grid.Column style={{ maxWidth: 500 }}>
-                    <Form>
-                        <Form.Field>
-                            <input placeholder='Lisk32 Address' />
-                        </Form.Field>
-                        <Form.Field>
-                            <input placeholder='Amount (1 = 10^8 tokens):' />
-                        </Form.Field>
-                        <Button type='submit' fluid size='large' style={{ backgroundColor: '#2BD67B', color: 'white' }}>Submit</Button>
-                    </Form>
-                </Grid.Column>
-            </Grid>
-        </div>
+            <Container>
+                <h2>Faucet</h2>
+                <p>The faucet transfers tokens from the genesis account to another.</p>
+                <Divider></Divider>
+                <div>
+                    <div class="ui two column doubling stackable grid container">
+                        <div class="column">
+                            <Form>
+                                <Form.Field>
+                                    <label>Recipient's Lisk32 Address:</label>
+                                    <input placeholder="Recipient's Lisk32 Address" id="address" name="address" />
+                                </Form.Field>
+                                <Form.Field>
+                                    <label>Amount:</label>
+                                    <input placeholder='Amount (1 = 10^8 tokens)' id="amount" name="amount" />
+                                </Form.Field>
+                                <Form.Field class="field">
+                                    <label>Sender's private key:</label>
+                                    <input placeholder="Private key of sender's account" type="password" id="privateKey" name="privateKey" />
+                                </Form.Field>
+                                <Button type='submit' fluid size='large' style={{ backgroundColor: '#2BD67B', color: 'white' }}>Submit</Button>
+                            </Form>
+                        </div>
+
+                        <div className='column'>
+                            <h3>Your transaction's details are:</h3>
+                            <div class="ui raised segment" style={{ overflow: 'scroll' }}>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+
+        </>
     );
 }
 
