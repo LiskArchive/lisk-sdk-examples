0ae4c14c249e77118d7b28f44e15ec8ff763f276
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2023-02-10 13:17:03 +0500

    Update faucet.js

--- guides/07-ui-boilerplate/hello_frontend/src/components/faucet.js
+++ guides/07-ui-boilerplate/hello_frontend/src/components/faucet.js
@@ -21,21 +21,11 @@ function Faucet() {
                                     <label>Amount:</label>
                                     <input placeholder='Amount (1 = 10^8 tokens)' id="amount" name="amount" />
                                 </Form.Field>
-                                <Form.Field class="field">
-                                    <label>Sender's private key:</label>
-                                    <input placeholder="Private key of sender's account" type="password" id="privateKey" name="privateKey" />
-                                </Form.Field>
                                 <Button type='submit' fluid size='large' style={{ backgroundColor: '#2BD67B', color: 'white' }}>Submit</Button>
                             </Form>
                         </div>
 
                         <div className='column'>
-                            <h3>Your transaction's details are:</h3>
-                            <div class="ui raised segment" style={{ overflow: 'scroll' }}>
-                                <>
-
-                                </>
-                            </div>
                         </div>
                     </div>
                 </div>
