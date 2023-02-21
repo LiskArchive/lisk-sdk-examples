6354b900ffa6e0a8391447310933ec40a147bc66
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2023-02-21 13:41:19 +0500

    Update faucet.js

--- guides/07-ui-boilerplate/hello_frontend/src/components/faucet.js
+++ guides/07-ui-boilerplate/hello_frontend/src/components/faucet.js
@@ -10,22 +10,20 @@ function Faucet() {
                 <p>The faucet transfers tokens from the genesis account to another.</p>
                 <Divider></Divider>
                 <div>
-                    <div class="ui two column doubling stackable grid container">
-                        <div class="column">
+                    <div className="ui two column doubling stackable grid container">
+                        <div className="column">
                             <Form>
                                 <Form.Field>
                                     <label>Recipient's Lisk32 Address:</label>
                                     <input placeholder="Recipient's Lisk32 Address" id="address" name="address" />
-                                </Form.Field>
-                                <Form.Field>
-                                    <label>Amount:</label>
-                                    <input placeholder='Amount (1 = 10^8 tokens)' id="amount" name="amount" />
                                 </Form.Field>
                                 <Button type='submit' fluid size='large' style={{ backgroundColor: '#2BD67B', color: 'white' }}>Submit</Button>
                             </Form>
                         </div>
 
                         <div className='column'>
+                            <>
+                            </>
                         </div>
                     </div>
                 </div>
