0ae20fe04d7dcc038a4fb9666f547ba2ece018cb
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2023-02-21 13:40:15 +0500

    Update sendHello.js

--- guides/07-ui-boilerplate/hello_frontend/src/components/sendHello.js
+++ guides/07-ui-boilerplate/hello_frontend/src/components/sendHello.js
@@ -10,29 +10,32 @@ function SendHello() {
                     <h2>Send Hello Message</h2>
                     <p>Please fill the following form to send a "Hello" message.</p>
                     <Divider></Divider>
-                    <div class="ui two column doubling stackable grid container">
-                        <div class="column">
-                            <Form class="ui form">
-                                <Form.Field class="field">
+                    <div className="ui two column doubling stackable grid container">
+                        <div className="column">
+                            <Form className="ui form">
+                                <Form.Field className="field">
                                     <label>Your message:</label>
                                     <input placeholder="Hello Message" id="hello" name="hello" />
                                 </Form.Field >
-                                <Form.Field class="field">
+                                <Form.Field className="field">
                                     <label>Fee:</label>
                                     <input placeholder='Fee (1 = 10^8 tokens)' type="text" id="fee" name="fee" />
                                 </Form.Field>
-                                <Form.Field class="field">
+                                <Form.Field className="field">
                                     <label>Sender's Passphrase:</label>
                                     <input placeholder='Passphrase of the hello_client' id="passphrase" name="passphrase" type="password" />
                                 </Form.Field>
-                                <Form.Field class="field">
+                                <Form.Field className="field">
+                                    <div className="ui yellow segment">
+                                        <i className="lightbulb outline icon"></i>The <strong>Sender's keyPath</strong> value can be from <strong>0-102</strong>. A default value has been pre-filled, which can be changed accordingly.
+                                    </div>
                                     <label>Sender's keyPath:</label>
-                                    <div class="ui labeled input">
-                                        <div class="ui label">
+                                    <div className="ui labeled input">
+                                        <div className="ui label">
                                             m/44'/134'/
                                         </div>
                                         <input placeholder='Enter any number from 0-102' id="keyPath" name="keyPath" type="text" />
-                                        <div class="ui label">
+                                        <div className="ui label">
                                             '
                                         </div>
                                     </div>
@@ -40,8 +43,9 @@ function SendHello() {
                                 <Button type='submit' fluid size='large' style={{ backgroundColor: '#2BD67B', color: 'white' }}>Submit</Button>
                             </Form>
                         </div>
-
                         <div className='column'>
+                            <>
+                            </>
                         </div>
                     </div>
                 </div>
