68194925af977a438010b81df477d36b92898970
Tschakki <tschakki@posteo.de>
2023-03-14 12:26:04 +0100

    Update lisk client version

--- guides/calculate-minfee/index.js
+++ guides/calculate-minfee/index.js
@@ -1,7 +1,7 @@
 const { apiClient, transactions } = require('@liskhq/lisk-client');
 
 let clientCache;
-const nodeAPIURL = 'ws://localhost:8080/ws'
+const nodeAPIURL = 'ws://localhost:7887/rpc-ws'
 
 const getClient = async () => {
 	if (!clientCache) {
@@ -11,43 +11,25 @@ const getClient = async () => {
 };
 
 // Paste the passphrase of sender account here
-const passphrase = '';
+//const passphrase = '';
 
 const calcMinFee = async (client) => {
-	const signedTxWithSomeFee = await client.transaction.create({
-		moduleID: 1000,
-		assetID: 0,
-		fee: BigInt(transactions.convertLSKToBeddows("1")),
-		asset: {
-			helloString: "Hello World!"
+	console.log(client);
+	const tx = await client.transaction.create({
+		module: 'token',
+		command: 'transfer',
+		//fee: BigInt(transactions.convertLSKToBeddows("1")),
+		params: {
+			message: "Hello World!"
 		}
-	}, passphrase);
-
-	return client.transaction.computeMinFee(signedTxWithSomeFee);
-}
-
-const postTx = async (client, minFee) => {
-	const signedTx = await client.transaction.create({
-		moduleID: 1000,
-		assetID: 0,
-		fee: minFee,
-		asset: {
-			helloString: "Hello World!"
-		}
-	}, passphrase);
-
-	return client.transaction.send(signedTx).then(res => {
-		return res;
 	});
+
+	return client.transaction.computeMinFee(tx);
 }
 
 getClient().then(client => {
 	calcMinFee(client).then(minFee => {
-		console.log("Minimum fee: ", minFee);
-		postTx(client, minFee).then((res) => {
-			console.log(res);
-			process.exit(0);
-		});
+		console.log("The minimum fee for the given transaction is: ", minFee, " Beddows");
 	})
 })
 
