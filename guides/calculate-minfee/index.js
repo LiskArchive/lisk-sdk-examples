ee816dd079a09c365000488147ffbe852684ee60
Tschakki <tschakki@posteo.de>
2023-03-14 14:58:57 +0100

    Update script

--- guides/calculate-minfee/index.js
+++ guides/calculate-minfee/index.js
@@ -3,6 +3,7 @@ const { apiClient, transactions } = require('@liskhq/lisk-client');
 let clientCache;
 const nodeAPIURL = 'ws://localhost:7887/rpc-ws'
 
+// Connects to a node API
 const getClient = async () => {
 	if (!clientCache) {
 		clientCache = await apiClient.createWSClient(nodeAPIURL);
@@ -10,26 +11,26 @@ const getClient = async () => {
 	return clientCache;
 };
 
-const calcMinFee = async (client) => {
-	const tx = {
-		module: 'token',
-		command: 'transfer',
-		senderPublicKey: '1234567890123456789012345678901234567890123456789012345678901234',
-		nonce: 0,
-		fee: 0,
-		params: {
-			data: "Hello World!"
-		}
-	};
+// Transaction to calculate the minimum fee for
+const tx = {
+	module: 'token',
+	command: 'transfer',
+	senderPublicKey: '1234567890123456789012345678901234567890123456789012345678901234',
+	nonce: 0,
+	fee: 0,
+	params: {
+		amount:100000000,
+		tokenID: "0300000000000000",
+		recipientAddress: "lskycz7hvr8yfu74bcwxy2n4mopfmjancgdvxq8xz",
+		data: "Hello World!"
+	}
+};
 
-	return client.transaction.computeMinFee(tx);
-}
-
+// Calculate and return the minimum fee
 getClient().then(client => {
-	calcMinFee(client).then(minFee => {
-		console.log("The minimum fee for the given transaction is: ", minFee, " Beddows, i.e. ", transactions.convertBeddowsToLSK(minFee.toString()), " LSK.");
-		process.exit(0);
-	})
+	const minFee = client.transaction.computeMinFee(tx);
+	console.log("The minimum fee for the given transaction is: ", minFee, " Beddows, i.e. ", transactions.convertBeddowsToLSK(minFee.toString()), " LSK.");
+	process.exit(0);
 }).catch(error => {
 	console.log("Error: " + error);
 	process.exit(1);
