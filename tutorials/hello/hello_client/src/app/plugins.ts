e231f0d0386ece7619a271d5fa0dd43d826acea1
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2023-07-26 17:54:14 +0500

    Enabled dashboard plugin
    - Bump up versions of node & packages, including lisk-sdk: beta 4.
    - Enable dashboard plugin.
    - Enable allowedMethods property in custom config.

--- tutorials/hello/hello_client/src/app/plugins.ts
+++ tutorials/hello/hello_client/src/app/plugins.ts
@@ -1,8 +1,10 @@
 /* eslint-disable import/namespace */
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
+import { DashboardPlugin } from '@liskhq/lisk-framework-dashboard-plugin';
 import { HelloInfoPlugin } from "./plugins/hello_info/hello_info_plugin";
 
 export const registerPlugins = (app: Application): void => {
     app.registerPlugin(new HelloInfoPlugin());
+    app.registerPlugin(new DashboardPlugin());
 };
