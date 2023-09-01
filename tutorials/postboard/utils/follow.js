436b23d2958c30522dbb6511a75b64c864611fb2
Franco NG <franco.ng@lightcurve.io>
2023-09-01 15:30:33 +0200

    Add utils

---
+++ tutorials/postboard/utils/follow.js
@@ -0,0 +1,46 @@
+const {
+  apiClient,
+  cryptography,
+  transactions
+} = require('@liskhq/lisk-client');
+const fs = require("fs");
+
+const privateKey = JSON.parse(fs.readFileSync("./privateKey.json", "utf-8")).privateKey;
+
+const getClient = async () => {
+  if (!clientCache) {
+    clientCache = await apiClient.createIPCClient(`~/.lisk/postboard`);
+  }
+  return clientCache;
+};
+
+const sendTransaction = async (nonce) => {
+  const client = await getClient();
+
+  const tx = await client.transaction.create({
+    module: 'post',
+    command: 'follow',
+    nonce: nonce,
+    fee: BigInt(transactions.convertLSKToBeddows('0.002')),
+    params: {
+      account: "lskzbqjmwmd32sx8ya56saa4gk7tkco953btm24t8",
+    }
+  }, privateKey);
+
+  console.log(`nonce=${nonce}: Sending`);
+  await client.transaction.send(tx);
+};
+
+let clientCache;
+(async () => {
+  const client = await getClient();
+  let { nonce } = await client.invoke('auth_getAuthAccount', {
+    address: cryptography.address.getLisk32AddressFromAddress(cryptography.address.getAddressFromPrivateKey(Buffer.from(privateKey, 'hex'))),
+  });
+
+  nonce = BigInt(nonce);
+  console.log("Starting nonce:", nonce);
+
+  await sendTransaction(nonce);
+  process.exit(0);
+})();
