2690770d183a08afb2ae1b46ef1b9716987791e8
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2023-09-14 21:09:06 +0500

    Correct Scripts

--- guides/tx-creation-and-signing/api-client.js
+++ guides/tx-creation-and-signing/api-client.js
@@ -1,6 +1,6 @@
 const { apiClient } = require('@liskhq/lisk-client');
 
-const RPC_ENDPOINT = 'ws://localhost:7887/rpc-ws';
+const RPC_ENDPOINT = 'ws://127.0.0.1:7887/rpc-ws';
 let clientCache;
 
 const getClient = async () => {
